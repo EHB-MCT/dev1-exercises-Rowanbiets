790fd6858890a47319634951e277e12e993f70e5
Rowan Biets <rowan.biets@student.ehb.be>
2022-10-26 15:11:22 +0200

    COMMIT

--- 4-Iteration-for/4-1.js
+++ 4-Iteration-for/4-1.js
@@ -1,4 +1,4 @@
-mport context from "../../scripts/context.js";
+import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
 
 draw();
@@ -8,11 +8,14 @@ function draw() {
     context.strokeStyle = "white";
     context.fillStyle = "orange";
     context.fillRect(0, 0, 600, 300);
-    drawLines();
+    drawOefening();
 }
 
-function drawLines() {
-    for (let i = 0; i <= 30, i++)
+function drawOefening() {
+    for (let i = 0; i <= 30; i++) {
         Utils.drawLine(i * 10, 0, 600 - i * 10, 300);
-    Utils.drawLine()
+        Utils.drawLine(300 + i * 10, 0, 300 - i * 10, 300);
+        Utils.drawLine(0, i * 10, 600, 300 - i * 10);
+    }
+
 }
